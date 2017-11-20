646369dc60b3dfd97b730296bec8dcc5399d3d86
Mẫn <vanman.lqd@gmail.com>
2017-11-20 20:40:04 +0700

    Add Exercise 2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = ConvertJarrayToListObject(Get());
+            foreach (var listItem in list)
+            {
+                Console.WriteLine(listItem.ToString());
+            }
+            Console.ReadKey();
+        }
+
+        static JArray Get()
+        {
+            var uri = "https://jsonplaceholder.typicode.com/posts";
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    return JArray.Parse(client.DownloadString(uri));
+                }
+            }
+            catch (WebException we)
+            {
+                using (var sr = new StreamReader(we.Response.GetResponseStream()))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                    throw;
+                }
+            }
+        }
+        static List<RootObject> ConvertJarrayToListObject(JArray jArray)
+        {
+            var listObject = new List<RootObject>();
+            foreach (var jObject in jArray)
+            {
+                listObject.Add(JsonConvert.DeserializeObject<RootObject>(jObject.ToString()));
+            }
+            return listObject;
+        }
+    }
+}
---
+++ Exercise_2/RootObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class RootObject
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public override string ToString() => $"UserID: {UserId}\tId: {Id}\tTitle: {Title}\tBody: {Body}";
+    }
+}
