db4028a26ddcb385d21451100a222048b916bc79
Mẫn <vanman.lqd@gmail.com>
2017-11-21 23:03:37 +0700

    Add Exercise 6

---
+++ Exercise_6/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xmlPath = "books.xml";
+            var jsonPath = "books.json";
+            ConvertXmlToJson(xmlPath, jsonPath);
+            Console.ReadKey();
+        }
+
+        static void ConvertXmlToJson(string xmlFilePath, string jsonFilePath)
+        {
+            XmlDocument xmlFile = new XmlDocument();
+            xmlFile.LoadXml(ReadFromFile(xmlFilePath));
+            WriteToFile(JsonConvert.SerializeXmlNode(xmlFile), jsonFilePath);
+        }
+        static string ReadFromFile(string filePath)
+        {
+            string strOutput = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        strOutput += line + "\n";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return strOutput;
+        }
+
+        static void WriteToFile(string content, string filePath)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    sw.WriteLine(content);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
